667d9b9865e506557497b61c0785db3ef386d4b1
Gebek-999 <70769830+Gebek-999@users.noreply.github.com>
2023-11-30 19:14:35 +0300

    Update Lab1_1.cpp

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -1,32 +1,23 @@
-setlocale(LC_ALL,"Russian");
 #include <iostream>
 #include <math.h>
 
-
 using namespace std;
+double x,y;
+
+float gk(float x){
+    float y;
+        if  (x>=-10 && x<=-6) y=sqrt(4-pow((x+8),2))-2;
+            else if (x>-6 && x<=2) y=0.5*x+1;
+            else if (x>2 && x<=6) y = 0;
+        else y = pow((x-6),2);
+    return y;
+}
 
 int main(){
-    
-    while(true){
-    double x,y;
-    cout<< "Введите Х в диапазоне -10 до 8";
-    cin >> x;
-    if  (x>=-10 && x<=-6){
-        y=sqrt(4-pow((x+8),2))-2;
+    for (x=-10; x<=8; x++)
+    {
+     y = gk(x);
+     cout << "x="<< x <<  "\ty=" <<y <<endl;
     }
-    else if (x>-6 && x<=2){
-        y=0.5*x+1;
-    }
-    else if (x>2 && x<=6){
-        y = 0;
-    }
-    else if (x>6 && x<=8) {
-        y = pow((x-6),2);
-    }
-    else {
-        cout << "Число X не принадлежит данному диапазону";
-    }
-    cout<< "y=" << y;
-    }
-    return 0 ;
+    return 0;
 }
